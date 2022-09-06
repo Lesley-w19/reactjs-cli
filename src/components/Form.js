c3121b8d09b1b035280628853354600404c7dc1e
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-06 16:40:24 +0300

    Added the form component -- usestate is first to the props name then changed to the inut message witten in the form

---
+++ src/components/Form.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+
+const Form = (props) => {
+  //   console.log(props);
+  const { name } = props;
+  let [message, setMessage] = useState(name);
+  useEffect(() => {
+    // message = name;
+  }, [message]);
+  return (
+    <div className="form-wrapper">
+      <form>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => {
+            e.preventDefault();
+            setMessage(e.target.value);
+          }}
+        />
+      </form>
+      <div className="form__output">{message}</div>
+    </div>
+  );
+};
+
+export default Form;
