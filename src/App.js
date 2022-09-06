139251c413e2db564a64653db35dace35954b02a
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-06 16:41:18 +0300

    Added the Form component and passed in the name props

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 // import logo from './logo.svg';
-import Counter from './components/Counter';
-import Todos from './components/Todos';
+import Counter from "./components/Counter";
+import Todos from "./components/Todos";
+import Form from "./components/Form";
 
-const App = () =>{
-  return(
+const App = () => {
+  return (
     <div>
-        <Counter name="Counter example"/>
-    <Todos name="Lists rendering example"/>
+      <Counter name="Counter example" />
+      <Todos name="Lists rendering example" />
+      <Form name="Form example" />
     </div>
-  
-  )
-}
+  );
+};
 
 export default App;
