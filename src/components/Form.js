4b15b1164a8d9a6d959a743c163c78c6fba2360c
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-09-07 09:00:30 +0300

    Cnagned the class name of the parent class in theform component

--- src/components/Form.js
+++ src/components/Form.js
@@ -13,35 +13,40 @@ const Form = (props) => {
   }, []);
 
   return (
-    <div className="form-wrapper">
-      <form>
-        <input
-          type="text"
-          ref={elementInput}
-          value={message}
-          onChange={(e) => {
-            e.preventDefault();
-            setMessage(e.target.value);
-          }}
-        />
-      </form>
-      <div className="form__output">{message}</div>
-
-      <div className="par__wrapper">
+    <div className="form">
+      <div>
+        <form className="form__wrapper">
+          <input
+            type="text"
+            ref={elementInput}
+            value={message}
+            onChange={(e) => {
+              e.preventDefault();
+              setMessage(e.target.value);
+            }}
+          />
+        </form>
+        <div className="form__wrapper__output">{message}</div>
+      </div>
+      <div>
         <button>Dark mode toggle</button>
-        <button onClick={() => setShow(true)}>Show Paragragh</button>
-        {show ? (
-          <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. At unde
-            necessitatibus, sed, dolor earum illo voluptatem adipisci quia
-            officia eveniet voluptatum est ea soluta! Neque, magnam vel dolore,
-            nisi corrupti maxime voluptates aut numquam hic saepe quas sunt
-            voluptatem cupiditate perspiciatis mollitia, similique qui beatae
-            fugiat vero! Labore, tempore laudantium.
-          </p>
-        ) : (
-          ""
-        )}
+      </div>
+      <div className="content__wrapper">
+        <div className="par__wrapper">
+          <button onClick={() => setShow(true)}>Show Paragragh</button>
+          {show ? (
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. At unde
+              necessitatibus, sed, dolor earum illo voluptatem adipisci quia
+              officia eveniet voluptatum est ea soluta! Neque, magnam vel
+              dolore, nisi corrupti maxime voluptates aut numquam hic saepe quas
+              sunt voluptatem cupiditate perspiciatis mollitia, similique qui
+              beatae fugiat vero! Labore, tempore laudantium.
+            </p>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </div>
   );
